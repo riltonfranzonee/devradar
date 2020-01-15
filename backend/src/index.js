49575bece0cabf6870bae4a01d0d9493b9d4c96b
Rilton Franzone <riltonfranzone@gmail.com>
2020-01-15 13:19:57 -0300

    web version ok

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+const express = require ('express') 
+const mongoose = require('mongoose')
+const cors = require('cors')
+const routes = require('./routes')
+
+const app = express()
+
+mongoose.connect('mongodb+srv://rilton:rilton@cluster0-rpmyl.mongodb.net/week10?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+app.use(cors())
+app.use(express.json())
+app.use(routes)
+
+
+app.listen(3333)
